14772870deac33bb047a46a3bb27ac76c493c372
Muhammad Ilham Askari <ilham.pezek@gmail.com>
2019-05-01 20:22:30 +0800

    First Commit

---
+++ app/src/main/java/picodiploma/dicoding/myapplication/DescribeActivity.java
@@ -0,0 +1,30 @@
+package picodiploma.dicoding.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DescribeActivity extends AppCompatActivity {
+
+    TextView TvDescName, TvDesc;
+    ImageView Picture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_describe);
+
+        Picture = findViewById(R.id.image_view);
+        TvDescName = findViewById(R.id.tv_desc_name);
+        TvDesc = findViewById(R.id.tv_desc);
+
+        String name = getIntent().getStringExtra("Name");
+        String desc = getIntent().getStringExtra("Desc");
+        int image = getIntent().getIntExtra("Pict", 0);
+
+        TvDescName.setText(name);
+        TvDesc.setText(desc);
+        Picture.setImageResource(image);
+    }
+}
